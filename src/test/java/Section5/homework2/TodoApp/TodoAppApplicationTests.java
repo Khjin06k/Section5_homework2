358e219227aa7cfc1fe4e05b0641cdc94cf50671
김희진 <khj4209kk@gmail.com>
2022-12-18 11:58:55 +0900

    init

---
+++ src/test/java/Section5/homework2/TodoApp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Section5.homework2.TodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
