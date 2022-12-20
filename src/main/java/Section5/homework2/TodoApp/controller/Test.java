891e9fa92ec884b83fe4bfa3d0dc483f40af5ccf
김희진 <khj4209kk@gmail.com>
2022-12-20 17:30:47 +0900

    a

--- src/main/java/Section5/homework2/TodoApp/controller/Test.java
+++ src/main/java/Section5/homework2/TodoApp/controller/Test.java
@@ -1,2 +1,13 @@
-package Section5.homework2.TodoApp.controller;public class Test {
+package Section5.homework2.TodoApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @GetMapping("/v1")
+    public String HelloWorld(){
+        return "To do Application!!, 어떻게 하는건지 진짜 모르겠다,,,";
+    }
 }
